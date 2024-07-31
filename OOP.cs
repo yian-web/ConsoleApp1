dc5977cac62f7eca0ec74bcbf2012d64339af290
yian-web <yianlintaipei@gmail.com>
2024-07-31 17:08:49 +0800

    Initial commit

---
+++ OOP.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class OOP
+    {
+        public void main()
+        {
+            //ElectricCar.main(); // Q1
+            //Derived1 b = new DeriDerived1(); //Q2
+            //Four.main();//03
+            //InfiniteMath.main();//04
+            //TestParent.main();//05
+            //Console.WriteLine(new delegateTest1().func3(1, 2, 4, 8));//06
+
+            //Console.WriteLine(new Class2().getValue());//07
+            //  Console.WriteLine(new Class2().funtion1());
+            //Console.WriteLine(Class2.Value);
+            //new Q08B().PrintFields();//08
+            //Q09B.MainQ09B();//09
+
+            //MDelegate m1 = new MDelegate(DM.Display);//10
+            //MDelegate m2 = new MDelegate(DM.print);
+            //MDelegate m3 = m1 + m2;
+            //MDelegate m4 = m2 + m1;
+            //MDelegate m5 = m3 - m2;
+            //m3();
+            //m4();
+            //m5();
+
+            //var speaker = new Speaker { Name = "Gaurav Kumar Arora" };//11
+            //var isTrue = speaker is Speaker;
+            //Console.WriteLine("speaker is of Speaker type:{0}", isTrue);
+
+            //var author = new Author { Name = "Gaurav Kumar Arora" };
+            //isTrue = speaker is Author;
+            //Console.WriteLine("speaker is of Author type:{0}", isTrue);
+
+            //var authorName = GetAuthorName(author);
+            //Console.WriteLine("Author name is:{0}", authorName);
+            //authorName = GetAuthorName(speaker);
+            //Console.WriteLine("Author name is:{0}", authorName);
+
+            //var anonymousData = new // 12
+            //
+            //    ForeName = "Fore",
+            //    SurName = "Sur"
+            //};
+            //Console.WriteLine("First Name : " + anonymousData.ForeName);
+        }
+        public static string GetAuthorName(dynamic obj)
+        {
+            Author authorObj = obj as Author;
+            return (authorObj != null) ? authorObj.Name : string.Empty;
+        }
+    }
+
+    public class Automobile
+    {
+        public String drive()
+        {
+            return "Driving vehicle";
+        }
+    }
+
+    public class Car : Automobile
+    {
+        public static int aa = 5;
+        public readonly int wheels = 2;
+        public virtual String drive()
+        {
+            return "Driving car";
+        }
+    }
+
+    public class ElectricCar : Car
+    {
+        public readonly int wheels = 4;
+        public sealed override String drive()
+        {
+            return "Driving an electric car";
+        }
+
+        public static void main()
+        {
+            Car car = new ElectricCar();
+            Console.WriteLine(car.drive());
+            Console.WriteLine(car.wheels);
+        }
+    }
+
+    class Base1
+    {
+        public Base1()
+        {
+            Console.WriteLine("Base");
+        }
+    }
+
+    class Derived1 : Base1
+    {
+        public Derived1()
+        {
+            Console.WriteLine("Derived");
+        }
+    }
+
+    class DeriDerived1 : Derived1
+    {
+        public DeriDerived1()
+        {
+            Console.WriteLine("DeriDerived");
+        }
+    }
+
+    class One
+    {
+        public One()
+        {
+            Console.WriteLine("One");
+        }
+    }
+    class Two : One
+    {
+        public Two()
+        {
+            Console.WriteLine("Two");
+        }
+    }
+    class Three : Two
+    {
+        public Three()
+        {
+            Console.WriteLine("Three");
+        }
+    }
+    public class Four
+    {
+        public static void main()
+        {
+            Three three = new Three();
+        }
+    }
+
+    class Math
+    {
+        public readonly double secret = 2;
+    }
+
+    class ComplexMath : Math
+    {
+        public readonly double secret = 4;
+    }
+
+    class InfiniteMath : ComplexMath
+    {
+        public readonly double secret = 8;
+        public static void main()
+        {
+            Math math = new InfiniteMath();
+            Console.WriteLine(math.secret);
+
+        }
+    }
+
+    class Parent
+    {
+        public void className()
+        {
+            Console.WriteLine("Parent");
+        }
+    }
+    class Child : Parent
+    {
+        void className()
+        {
+            Console.WriteLine("Child");
+        }
+    }
+
+    public class TestParent
+    {
+        public static void main()
+        {
+            Parent parent = new Child();
+            parent.className();
+        }
+    }
+
+    public class delegateTest1
+    {
+        public Func<int, int, int, int, int> func3 = (x, y, a, b) => { return x + y + a + b; };
+    }
+
+
+
+    public class Class2 : abClass1, inter1
+    {
+        public static int Value = 2;
+        public override int getValue()
+        {
+            return 2;
+        }
+
+        public int funtion1()
+        {
+            return base.getValue();
+        }
+    }
+    interface inter1
+    {
+        int funtion1();
+    }
+
+    public abstract class abClass1 : abClass0
+    {
+        public static int Value = 1;
+        public override int getValue()
+        {
+            return 1;
+        }
+        public int funtion1()
+        {
+            return getValue();
+        }
+    }
+
+    public abstract class abClass0
+    {
+        public abstract int getValue();
+        public static int Value = 0;
+    }
+
+
+    class Q08A
+    {
+        public Q08A()
+        {
+            PrintFields();
+        }
+        public virtual void PrintFields() { }
+    }
+    class Q08B : Q08A
+    {
+        int x = 1;
+        int y;
+        public Q08B()
+        {
+            y = -1;
+        }
+        public override void PrintFields()
+        {
+            Console.WriteLine("x={0},y={1}", x, y);
+        }
+    }
+
+    class Qo9A
+    {
+        public static int X;
+        static Qo9A()
+        {
+            X = Q09B.Y + 1;
+        }
+    }
+    class Q09B
+    {
+        public static int Y = Qo9A.X + 1;
+        static Q09B() { }
+        public static void MainQ09B()
+        {
+            Console.WriteLine("X={0},Y={1}", Qo9A.X, Q09B.Y);
+        }
+    }
+
+    delegate void MDelegate();
+    class DM
+    {
+        static public void Display()
+        {
+            Console.WriteLine("Display");
+        }
+        static public void print()
+        {
+            Console.WriteLine("print");
+        }
+    }
+
+    class Speaker
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+    class Author
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,370 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new delegateTest();
+
+            // function Delegate 
+            //var myDelegateTest = new delegateTest();
+            //Console.WriteLine(myDelegateTest.func3(1, 2, 3, 4));
+
+            // set target method
+            //MyDelegate del = new MyDelegate(MethodA);
+            // del = MethodA;
+            //// or set lambda expression 
+            MyDelegate del = (string msg) => Console.WriteLine(msg);
+            del("hello world");
+
+            //AlarmClock clock = new AlarmClock();
+            //// 訂閱事件，指定處理程序
+            //clock.Alarm += (sender, e) =>
+            //{
+            //    Console.WriteLine("時間到了，該起床了！");
+            //};
+            //// 模擬鬧鐘觸發
+            //clock.TriggerAlarm();
+            //
+            //Derived b = new DeriDerived();
+
+            //Car car = new ElectricCar();
+            //Console.WriteLine(car.drive());
+
+            //One one = new Two();
+            //one.print();
+
+            //Parent parent = new Child();
+            //parent.className();
+
+            // int aaI = Int32("5")
+
+            //Inface inface = new Inface(5);
+            //Console.WriteLine(inface.geta());
+
+            //new SomeY(20).doYourWork();
+
+            //new Seal().func();
+            //new OOP().main();
+
+
+            //new ThreadExm().doFun();
+            Console.ReadKey();
+        }
+
+        class AlarmClock
+        {
+            // 定義事件
+            public event EventHandler Alarm;
+            // 模擬鬧鐘觸發事件
+            public void TriggerAlarm()
+            {
+                Console.WriteLine("鬧鐘響了！");
+                OnAlarm(); // 觸發事件
+            }
+            // 處理事件的方法
+            protected virtual void OnAlarm()
+            {
+                Alarm?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        void enumerable()
+        {
+            int[] numbers = { 0, 30, 20, 15, 90, 85, 40, 75 };
+
+            IEnumerable<int> query = numbers.Where((number, index) => number <= index * 10);
+
+            foreach (int number in query)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        void ActionTDelegate()
+        {
+            List<string> names = new List<string>();
+            names.Add("Bruce");
+            names.Add("Alfred");
+            names.Add("Tim");
+            names.Add("Richard");
+
+            // Display the contents of the list using the Print method.
+            //names.ForEach(Print);
+
+            // The following demonstrates the anonymous method feature of C#
+            // to display the contents of the list to the console.
+
+            names.ForEach(delegate (string name)
+            {
+                Console.WriteLine(name);
+            });
+        }
+
+        public class delegateTest
+        {
+            //Callback handler = DelegateMethod;
+            //handler(1, 2, 3, 4);
+
+            //Func<string> func = () => "Hello World";
+
+            //Func<int, int> func1 = x => x + 1;
+
+            //Func<string, string, bool> func2 = (x, y) => x.StartsWith(y);
+
+            public Func<int, int, int, int, int> func3 = (x, y, a, b) => { return x + y + a + b; };
+
+
+            //public Action func = ()=> Console.WriteLine("");
+
+            //public Action<int> func2 = (x) => Console.WriteLine(x);
+
+            //public Action<in T1, in T2>(T1 arg1, T2 arg2);
+        }
+
+        public delegate void Callback(int x, int y, int a, int b);
+        public void DelegateMethod(int x, int y, int a, int b)
+        {
+            Console.WriteLine(x + y + a + b);
+        }
+
+        public delegate TResult Func<out TResult>();
+
+        public delegate TResult Func<in T, out TResult>(T arg);
+
+        public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+
+        static int fun1(int a, int b)
+        {
+            return a + b;
+        }
+        static int fun2(int a, int b)
+        {
+            return a - b;
+        }
+
+        public delegate void MyDelegate(string msg); // declare a delegate
+
+        //static void MethodA(string message)
+        //{
+        //    Console.WriteLine(message);
+        //}
+    }
+
+
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base");
+        }
+    }
+    class Derived : Base
+    {
+        public Derived()
+        {
+            Console.WriteLine("Derived");
+        }
+    }
+
+    class DeriDerived : Derived
+    {
+        public DeriDerived()
+        {
+            Console.WriteLine("DeriDerived");
+        }
+    }
+
+
+    class One
+    {
+        public void print()
+        {
+            Console.WriteLine("1");
+        }
+    }
+
+    class Two : One
+    {
+
+        public void print()
+        {
+            Console.WriteLine("2");
+        }
+    }
+
+    class Parent
+    {
+        public void className()
+        {
+            Console.WriteLine("Parent");
+        }
+    }
+    class Child : Parent
+    {
+        void className()
+        {
+            Console.WriteLine("Child");
+        }
+    }
+
+    public class Car
+    {
+        public String drive()
+        {
+            return "Driving car";
+        }
+    }
+
+    public class ElectricCar : Car
+    {
+        public String drive()
+        {
+            return "Driving an electric car";
+        }
+    }
+
+    public class Inface : someI, someII
+    {
+        public Inface(int value) : base()
+        {
+            doYourValue2 = value;
+        }
+        public override void doYourWork()
+        {
+
+        }
+        public void doYourWork3()
+        {
+            //doYourValu3++;
+        }
+        //a= doYourValue3;
+        //int b {
+        //    get { base.doYourValue}
+        //    set;
+        //}
+    }
+    interface someII
+    {
+        void doYourWork3();
+    }
+
+    public abstract class someI : some0
+    {
+        //public abstract void doYourWork();
+
+        //public static int doYourValue = 3;
+        //public  int doYourValue2 = 3;
+        public someI()
+        {
+            //doYourValue2 = 2;
+        }
+    }
+
+    public abstract class some0
+    {
+        public abstract void doYourWork();
+
+        public static int doYourValue = 3;
+        public int doYourValue2 = 3;
+    }
+
+
+    public abstract class SomeX
+    {
+        public abstract void doYourWork();
+
+        public void doYourWor2()
+        {
+            Console.WriteLine("doYourValue2 = " + doYourValue2);
+            Console.WriteLine("doYourValue = " + doYourValue);
+        }
+
+        public int doYourValue2;
+        public static int doYourValue = 3;
+    }
+
+    public class SomeY : SomeX
+    {
+        public SomeY(int value) : base()
+        {
+            doYourValue2 = value;
+        }
+        public override void doYourWork()
+        {
+            base.doYourWor2();
+        }
+    }
+
+
+
+    public sealed class Seal
+    {
+        public void func()
+        {
+            try
+            {
+                int ii = Convert.ToInt32("123");
+                String str = Convert.ToString(123);
+                char ch = str.ToCharArray()[0];
+
+                for (int x = 1; x < 10; x++)
+                {
+                    for (int y = 1; y < 10; y++)
+                    {
+                        Console.Write(x + " * " + y + " = " + x * y + " ", 10);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    public class ThreadExm
+    {
+        private void FirstThreadFun()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + "   i =  " + i);
+
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " Done");
+        }
+
+        public void doFun()
+        {
+            Thread.CurrentThread.Name = "MainThread";
+            Thread testThread = new Thread(new ThreadStart(new ThreadExm().FirstThreadFun));
+            testThread.Name = "firstThread";
+
+            for (int z = 0; z < 20; z++)
+            {
+                if (z == 10)
+                {
+                    testThread.Start();
+                    testThread.Join();
+                }
+                else
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + "   z =  " + z);
+                }
+            }
+        }
+    }
+
+
+
+
+}
+
